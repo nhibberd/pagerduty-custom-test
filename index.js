8b96088a84d717c3ecbb5ecc3961d6e7365d1228
Nick Hibberd <nhibberd@gmail.com>
2020-06-10 19:03:18 -0700

    lib

---
+++ index.js
@@ -0,0 +1,34 @@
+const vm = require("vm");
+const fs = require('fs');
+
+var PD = {
+  inputRequest: {},
+  emitGenericEvents: jest.fn(),
+  assertType: jest.fn(),
+  fail: jest.fn()
+}
+
+exports.runTest = function(
+  transformFile,
+  inputRequest,
+  fn,
+) {
+  runTestWithFail(transformFile, inputRequest, fn, jest.fn())
+}
+
+exports.runTestWithFail = function(
+  transformFile,
+  inputRequest,
+  fn,
+  failFn,
+) {
+  var data = fs.readFileSync(transformFile);
+  PD.inputRequest = inputRequest
+  PD.emitGenericEvents = fn
+  var rawContext = {
+    PD: PD
+  }
+  var context = vm.createContext(rawContext);
+  var script = new vm.Script(data);
+  script.runInContext(context);
+}
--- test/test.js
+++ test/test.js
@@ -1,39 +1,15 @@
-var vm = require("vm");
-var fs = require("fs");
-
-var PD = {
-  inputRequest: {},
-  emitGenericEvents: jest.fn(),
-  assertType: jest.fn(),
-  fail: jest.fn()
-}
-
-var runTest = function(transformFile, inputRequest, fn) {
-  runTestWithFail(transformFile, inputRequest, fn, jest.fn())
-}
-
-var runTestWithFail = function(transformFile, inputRequest, fn, failFn) {
-  var data = fs.readFileSync(transformFile);
-  PD.inputRequest = inputRequest
-  PD.emitGenericEvents = fn
-  var rawContext = {
-    PD: PD
-  }
-  var context = vm.createContext(rawContext);
-  var script = new vm.Script(data);
-  script.runInContext(context);
-}
+var pdtest = require("../index.js");
 
 test('expect-throw', () => {
   expect(() => {
-    runTest("transform-body.js", '{}', jest.fn())
+    pdtest.runTest("test/transform-body.js", '{}', jest.fn())
   }).toThrow();
 });
 
 test('ok', () => {
   var emit = jest.fn()
   var fail = jest.fn()
-  runTestWithFail("transform.js", `{}`, emit, fail)
+  pdtest.runTestWithFail("test/transform.js", `{}`, emit, fail)
   expect(emit).toHaveBeenCalledTimes(1);
   expect(fail).toHaveBeenCalledTimes(0);
 });
